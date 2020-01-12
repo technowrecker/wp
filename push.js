2e86066eb1ef2ea50364c9b5d7c14fc81890df4b
Rashid <technowreckers2@gmail.com>
2020-01-12 12:24:37 +0500

    fix

--- push.js
+++ push.js
@@ -8,5 +8,12 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {}
+let sub = {
+    "endpoint": "https://fcm.googleapis.com/fcm/send/f-Tv9J75JCQ:APA91bGsGaNmaXNGcunhPi9ISS19P8VACdu_UVB6zShXN8-Q5nY-wp11-TW7vccH-uFLP5fyluwYKLL52tlvNsMtyatN_E3Ef6tN2ySeFCqHuqFMSSDQJgyZPcAeRb1j2LznfC9A0gEW",
+    "expirationTime": null,
+    "keys": {
+        "p256dh": "BFfYFACY8cUHPWcNDDMIsA6UCpnGLshbc3EPTkcSoDqgpFiiA1uDFCwHQRmDCm8YywJMkU45w74Zm2o6rAJRjqU",
+        "auth": "XIQv8IjRVL-lX_Mp7up2kg"
+    }
+}
 push.sendNotification(sub, 'text message')
